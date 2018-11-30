281e5b294fcaef615e9b860a2b94ff698efde968
claudius <claudius.teodorescu@gmail.com>
2018-11-30 13:13:18 +0200

    Added ModuleDescription class.

---
+++ src/main/java/ro/kuberam/libs/java/digitalPublishing/ModuleDescription.java
@@ -0,0 +1,15 @@
+package ro.kuberam.libs.java.digitalPublishing;
+
+
+/**
+ * Module description.
+ *
+ * @author <a href="mailto:claudius.teodorescu@gmail.com">Claudius Teodorescu</a>
+ */
+public class ModuleDescription {
+    public static final String VERSION = "1.0.0";
+    public static final String NAMESPACE_URI = "http://expath.org/ns/crypto/digital-publishing/";
+    public static final String PREFIX = "digi-pub";
+    public static final String MODULE_NAME = "Library for EXPath Digital Publishing Module";
+    public static final String MODULE_DESCRIPTION = "A ";
+}
