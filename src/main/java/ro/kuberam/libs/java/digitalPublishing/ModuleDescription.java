e2350d3fbdfda4000956412144d290d7aceb332c
claudius <claudius.teodorescu@gmail.com>
2018-12-12 16:09:55 +0200

    Fixed NAMESPACE_URI.

--- src/main/java/ro/kuberam/libs/java/digitalPublishing/ModuleDescription.java
+++ src/main/java/ro/kuberam/libs/java/digitalPublishing/ModuleDescription.java
@@ -8,7 +8,7 @@ package ro.kuberam.libs.java.digitalPublishing;
  */
 public class ModuleDescription {
     public static final String VERSION = "1.0.0";
-    public static final String NAMESPACE_URI = "http://expath.org/ns/crypto/digital-publishing/";
+    public static final String NAMESPACE_URI = "http://expath.org/ns/digital-publishing/";
     public static final String PREFIX = "digi-pub";
     public static final String MODULE_NAME = "Library for EXPath Digital Publishing Module";
     public static final String MODULE_DESCRIPTION = "A ";
